7b6fbc71e187f731e82bf9313c0fc255a1899e6d
Karishad <kieronyin@gmail.com>
2021-03-13 20:22:38 -0600

    Showing how to build a basic webscraper

--- LearningBS4Webscrap/TestBS4.py
+++ LearningBS4Webscrap/TestBS4.py
@@ -1,5 +1,11 @@
+# Kieron Yin
+# 3/13/2021
+# A basic webscraper to grab titles from the NPR website
 from bs4 import BeautifulSoup
 import requests
 
 source = requests.get('https://www.npr.org/').text
-print(source)
+soup = BeautifulSoup(source, 'lxml')
+for titles in soup.find_all('h3', class_='title'):
+    cleanTitle = titles.text
+    print(cleanTitle)
